143450285bd4f11f627f814f9f4014a6778b5c5f
Maarten Visscher <mail@maartenvisscher.nl>
2016-02-07 14:05:57 +0100

    Added Bootstrap and jQuery.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,6 +29,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/dependencies/jquery-1.12.0.min.js',
   'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
