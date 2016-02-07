9b0ffe310050da45a7b08b8417173c5d15ea1041
Maarten Visscher <mhvis@users.noreply.github.com>
2016-02-07 20:18:32 +0100

    Added empty Animal, Cell, Game, Player API.

---
+++ api/controllers/CellController.js
@@ -0,0 +1,11 @@
+/**
+ * CellController
+ *
+ * @description :: Server-side logic for managing cells
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
