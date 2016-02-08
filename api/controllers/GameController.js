a75f18303d2e9b39316b3fdc9e91234853a68f66
Maarten Visscher <mhvis@users.noreply.github.com>
2016-02-08 19:55:23 +0100

    Added new game route && cleaned homepage.ejs.

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -7,7 +7,15 @@
 
 module.exports = {
 	new: function(req, res) {
-
+		var width = req.param("width");
+		var height = req.param("height");
+		var animalCount = req.param("animalCount");
+		Game.new(width, height, animalCount, function(err, game) {
+			if (err) {
+				return res.serverError(err);
+			}
+			res.ok(game);
+		});
 	},
 	
 };
