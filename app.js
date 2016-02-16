851a23acefe4341f913701b40df8b20af7493171
Maarten Visscher <mail@maartenvisscher.nl>
2016-02-16 22:21:49 +0100

    Removed Angular/Feathers/Bootstrap, initiated express

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-var feathers = require('feathers');
-var bodyParser = require('body-parser');
-var memory = require('feathers-memory');
+var express = require('express');
+var app = express();
 
-var app = feathers();
-
-app.configure(feathers.rest());
-app.configure(feathers.socketio());
-
-app.use(bodyParser.json());
-
-app.use('/game', memory());
-
-app.use('/', feathers.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.listen(3000, function() {
-    console.log('Listening');
+	console.log('Listening on port 3000');
 });
